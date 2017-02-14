1bb6eaf6ea804350a8c3b472d81f778a0384df76
Charles Peden and Dylan Stackhouse <charles@>
2017-02-14 13:18:56 -0800

    fix rounding issue when displaying pennies

--- src/Coin.php
+++ src/Coin.php
@@ -5,26 +5,54 @@
         {
             $output_array = array();
             $change = $input;
-            if($change > 0.25)
+            if($change >= 25)
             {
-                $quarters =floor($change/0.25);
-                array_push($output_array, (int) $quarters);
-                $change -= $quarters * 0.25;
+                $quarters =(int)($change/25);
+                array_push($output_array, $quarters);
+                $change -= $quarters * 25;
+            } else {
+                array_push($output_array, 0);
             }
-            if($change > 0.10)
+            if($change >= 10)
             {
-                $dimes = floor($change/0.10);
-                array_push($output_array, (int) $dimes);
-                $change -= $dimes * 0.10;
+                $dimes = (int)($change/10);
+                array_push($output_array, $dimes);
+                $change -= $dimes * 10;
+            } else {
+                array_push($output_array, 0);
             }
-            if($change > 0.05)
+            if($change >= 05)
             {
-                $nickels = floor($change/0.05);
-                array_push($output_array, (int) $nickels);
-                $change -= $nickels * 0.05;
+                $nickels = (int)($change/05);
+                array_push($output_array, $nickels);
+
+                var_dump($change);
+                var_dump($nickels);
+                $change -= $nickels * 05;
+                var_dump($change);
+            } else {
+                array_push($output_array, 0);
             }
 
-            return $output_array;
+            if($change >= 01)
+            {
+                $pennies = (int) $change/01;
+                var_dump($pennies);
+                array_push($output_array,  $pennies);
+                var_dump($change);
+                var_dump($pennies);
+                $change -= $pennies * 01;
+                var_dump($change);
+            } else {
+                array_push($output_array, 0);
+            }
+
+                return $output_array;
+
+
         }
     }
+
+    $coin = new Coin;
+    $coin->getCoinage(43);
 ?>
--- tests/CoinTest.php
+++ tests/CoinTest.php
@@ -5,27 +5,34 @@
         function test_quarters()
         {
             $test_Coin = new Coin;
-            $input1 = 0.43;
+            $input1 = 43;
 
             $result = $test_Coin->getCoinage($input1);
-            $this->assertEquals([1,1,1], $result);
+            $this->assertEquals([1,1,1,3], $result);
         }
         function test_dimes()
         {
             $test_Coin = new Coin;
-            $input1 = 0.43;
+            $input1 = 43;
 
             $result = $test_Coin->getCoinage($input1);
-            $this->assertEquals([1,1,1], $result);
+            $this->assertEquals([1,1,1,3], $result);
         }
         function test_nickels()
         {
             $test_Coin = new Coin;
-            $input1 = 0.43;
+            $input1 = 43;
 
             $result = $test_Coin->getCoinage($input1);
-            $this->assertEquals([1,1,1], $result);
+            $this->assertEquals([1,1,1,3], $result);
         }
+        function test_pennies()
+        {
+            $test_Coin = new Coin;
+            $input1 = 43;
 
+            $result = $test_Coin->getCoinage($input1);
+            $this->assertEquals([1,1,1,3], $result);
+        }
     }
 ?>
