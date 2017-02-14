0e2b9b6d4ebf424009d6a98b7e0d78352b9614f5
Charles Peden and Dylan Stackhouse <charles@>
2017-02-14 11:08:06 -0800

    add test for correct amount of dimes and code to pass test

--- src/Coin.php
+++ src/Coin.php
@@ -7,8 +7,15 @@
             $change = $input;
             if($change > 0.25)
             {
-                array_push($output_array, floor($change/0.25));
-                $change -= floor($change/0.25);
+                $quarters =floor($change/0.25);
+                array_push($output_array, (int) $quarters);
+                $change -= $quarters * 0.25;
+            }
+            if($change > 0.10)
+            {
+                $dimes = floor($change/0.10);
+                array_push($output_array, (int) $dimes);
+                $change -= $dimes * 0.10;
             }
             return $output_array;
         }
--- tests/CoinTest.php
+++ tests/CoinTest.php
@@ -5,10 +5,19 @@
         function test_quarters()
         {
             $test_Coin = new Coin;
-            $input1 = 0.46;
+            $input1 = 0.43;
 
             $result = $test_Coin->getCoinage($input1);
-            $this->assertEquals([1], $result);
+            $this->assertEquals([1,1], $result);
         }
+        function test_dimes()
+        {
+            $test_Coin = new Coin;
+            $input1 = 0.43;
+
+            $result = $test_Coin->getCoinage($input1);
+            $this->assertEquals([1,1], $result);
+        }
+
     }
 ?>
